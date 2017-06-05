5886598feabfc81a0b2d298a6bac65bf0a130527
安 卢 <laa6203@163.com>
2017-06-05 10:10:46 +0800

    add imread and imwrite

--- SGTools2/src/SGTools2.cpp
+++ SGTools2/src/SGTools2.cpp
@@ -7,9 +7,30 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include "opencv2/opencv.hpp"
+
 using namespace std;
+using namespace cv;
+
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "---SGTools2---" << endl; // prints !!!Hello World!!!
+
+	String fn0 = "t10/Sat Jun 03 15-32-54.bmp";
+	String fn1 = "t11/Sat Jun 03 15-32-54.png";
+
+	Mat A = imread(fn0,1);
+	if(A.empty()){
+		cout << "load file error!" << endl;
+		exit(-1);
+	}
+
+
+	//imshow("SGTools2", A);
+	//waitKey(1000);
+	imwrite(fn1,A);
+
+	cout << "---end SGTools2---" <<endl;
 	return 0;
 }
